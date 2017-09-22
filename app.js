09a475a2bd6cb161775e33142005c5054d9dd5ca
Eren Erdogan <ererdogan20@gmail.com>
2017-09-22 17:08:19 -0400

    All Database Setup, ready for routing

--- app.js
+++ app.js
@@ -18,3 +18,20 @@ nunjucks.configure('views', {
 });
 
 // app.use('/', routes);
+// Where your server and express app are being defined:
+
+var models = require('./models');
+
+// ... other stuff
+
+models.User.sync({})
+.then(function () {
+    return models.Page.sync({});
+})
+.then(function () {
+    // make sure to replace the name below with your express app
+    app.listen(3000, function () {
+        console.log('Server is listening on port 3000!');
+    });
+})
+.catch(console.error);
